96759c7bdb8c0dbbe25fa26ef223667e18cdea84
duanbm1911 <bmdhou@gmail.com>
2020-12-14 11:47:38 +0700

    modified:   system_monitoring/system_monitoring/settings.py

--- system_monitoring/system_monitoring/settings.py
+++ system_monitoring/system_monitoring/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+075#t_^b+m!rhdxby5rhum(q8jjvatzb6)o0o8*8z_))gaige'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
