544fbef4c4755967c0ec94a6b4bc715effcbeb0b
duanbm1911 <bmdhou@gmail.com>
2020-12-29 10:47:42 +0700

    modified:   system_monitoring/__pycache__/settings.cpython-38.pyc 	modified:   system_monitoring/settings.py 	accounts/ 	api/__pycache__/ 	api/migrations/0001_initial.py api/migrations/__pycache__/ 	api/serializers.py 	api/urls.py

--- system_monitoring/system_monitoring/settings.py
+++ system_monitoring/system_monitoring/settings.py
@@ -84,18 +84,18 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'syslog',
-    #     'USER': 'root',
-    #     'PASSWORD': 'Admin@123',
-    #     'HOST': '192.168.169.184',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'syslog',
+        'USER': 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': '192.168.169.184',
+        'PORT': '3306',
+    }
 }
 
 
