85b75203a79f9990264bfd2946c1f13542eea13b
duanbm1911 <bmdhou@gmail.com>
2020-12-29 15:31:57 +0700

    commit2

--- system_monitoring/api/views.py
+++ system_monitoring/api/views.py
@@ -19,9 +19,11 @@ def api_log_request(request):
     if request.method == 'GET':
         start_date = request.query_params.get('start_date')
         end_date = request.query_params.get('end_date')
+        start_time = request.query_params.get('start_time')
+        end_time = request.query_params.get('end_time')
         device = request.query_params.get('device')
         log = request.query_params.get('log')
-        model = SystemLogModel.objects.filter(date__range=[start_date,end_date], device__contains=device , log__contains=log)
+        model = SystemLogModel.objects.filter(date__range=[start_date,end_date], time__range=[start_time, end_time], device__contains=device , log__contains=log)
         serializer = SystemLogSerializer(model, many=True)
         data = {'message':'OK', 'data':serializer.data}
         return Response(data, status=status.HTTP_200_OK)
